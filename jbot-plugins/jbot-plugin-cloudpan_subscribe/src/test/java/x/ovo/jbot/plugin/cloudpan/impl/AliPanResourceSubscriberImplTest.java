27f2537f32f7724cff7bcbf29b738c0b442570e9
gitsilence <gitsilence@lacknb.cn>
2025-04-13 11:47:12 +0800

    update

--- jbot-plugins/jbot-plugin-cloudpan_subscribe/src/test/java/x/ovo/jbot/plugin/cloudpan/impl/AliPanResourceSubscriberImplTest.java
+++ jbot-plugins/jbot-plugin-cloudpan_subscribe/src/test/java/x/ovo/jbot/plugin/cloudpan/impl/AliPanResourceSubscriberImplTest.java
@@ -3,6 +3,7 @@ package x.ovo.jbot.plugin.cloudpan.impl;
 import org.junit.jupiter.api.Test;
 import x.ovo.jbot.plugin.model.SubscribeModel;
 
+import java.time.DayOfWeek;
 import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -28,9 +29,12 @@ class AliPanResourceSubscriberImplTest {
 
     @Test
     public void listTest() {
-        var url = "https://www.alipan.com/s/v1bBBEcNf9p/folder/67cd7c007ab50c0ee60c4f6187c1cd10117c33d2";
+        var url = "https://www.alipan.com/s/oEhBnRkwGfG/folder/67d0126819c63168c8494be99d936c623f27841d";
         SubscribeModel subscribeModel = new SubscribeModel();
         subscribeModel.setResourceUrl(url);
+        subscribeModel.setPassword("6666");
+        subscribeModel.setDayOfWeek(DayOfWeek.SUNDAY);
+        subscribeModel.setCronTime("09:00");
         AliPanResourceSubscriberImpl subscriber = new AliPanResourceSubscriberImpl();
         subscriber.checkForUpdate(subscribeModel);
     }
